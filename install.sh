2a27ed0ff33e3e1e2dda4884da0da42fbb401ddd
Evan Wappel <ewappel@gmail.com>
2017-10-25 16:52:40 -0700

    Update and rename script to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:team-xbmc/ppa
-sudo apt-get update
-sudo apt-get install kodi
+sudo apt-get -y update
+sudo apt-get -y install kodi
