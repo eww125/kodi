09cad46e65a9e6c396afeefd07c5daf83d94cdd9
Evan Wappel <ewappel@gmail.com>
2017-10-25 16:59:26 -0700

    Update and rename repo to download_repo.sh

--- download_repo.sh
+++ download_repo.sh
@@ -1,2 +1,4 @@
 wget http://srp.nu/krypton/repositories/superrepo/superrepo.kodi.krypton.repositories-0.7.04.zip
 wget http://srp.nu/krypton/repositories/others/superrepo.kodi.krypton.external.repositories-0.7.04.zip
+wget http://noobsandnerds.com/portal/noobsandnerds%20Repo%20(2.3.0).zip
+wget http://noobsandnerds.com/portal/*REPOSITORY%20PORTAL/*.*
