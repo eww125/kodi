b85e8e0008c966c86b54bcd5f761140b1e1be5a6
Evan Wappel <ewappel@gmail.com>
2017-10-26 16:46:44 -0700

    noob_repo

--- noob_repo.py
+++ noob_repo.py
@@ -9,11 +9,14 @@ for x in range(0,len(split1)):
         file_list.append(split1[x][1:(split1[x].find(".zip")+4)])
 
 #print file_list[0]
+from os.path import expanduser
+home = expanduser("~")
+print home
 
 for x in range(0,len(file_list)):
     url = "http://noobsandnerds.com/portal/*REPOSITORY%20PORTAL/" + file_list[x]
     response = urllib2.urlopen(url)
     zipcontent = response.read()
-    print "writing " +  file_list[x] + " ..."
-    with open(file_list[x], 'w') as f:
+    print "writing " +  home + "/kodi_ubuntu/repo/" + file_list[x] + " ..."
+    with open(home + "/kodi_ubuntu/repo/" + file_list[x], 'w') as f:
 	       f.write(zipcontent)
