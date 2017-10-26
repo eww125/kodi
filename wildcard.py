c6384692783412c47092f05103bdd7007848a909
Evan Wappel <ewappel@gmail.com>
2017-10-25 20:59:44 -0700

    repo

--- wildcard.py
+++ wildcard.py
@@ -1,8 +1,19 @@
-from bs4 import BeautifulSoup
 import urllib2
+url = "http://noobsandnerds.com/portal/*REPOSITORY%20PORTAL/"
+response = urllib2.urlopen(url)
+scraped = response.read()
+split1 = scraped.split("href=")
+file_list=[]
+for x in range(0,len(split1)):
+    if split1[x].find(".zip") != -1:
+        file_list.append(split1[x][1:(split1[x].find(".zip")+4)])
 
-resp = urllib2.urlopen(""http://noobsandnerds.com/portal/*REPOSITORY%20PORTAL"")
-soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
+#print file_list[0]
 
-for link in soup.find_all('a', href=True):
-    print link['href']
+for x in range(0,len(file_list)):
+    url = "http://noobsandnerds.com/portal/*REPOSITORY%20PORTAL/" + file_list[x]
+    response = urllib2.urlopen(url)
+    zipcontent = response.read()
+    print "writing " +  file_list[x] + " ..."
+    with open(file_list[x], 'w') as f:
+	       f.write(zipcontent)
