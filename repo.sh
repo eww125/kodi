7fe32304ad6c6794a32dd88c8625fea017c99986
Evan Wappel <ewappel@gmail.com>
2018-05-24 12:55:12 -0400

    Update and rename download_repo.sh to repo.sh

--- repo.sh
+++ repo.sh
@@ -3,3 +3,5 @@ wget http://repo.mrblamo.xyz/repository.PureRepo-0.0.4.zip
 wget http://repo.mrblamo.xyz/repository.blamo-0.1.3.zip
 wget http://repo.mrblamo.xyz/repository.griffin-0.0.7.zip
 wget http://repo.mrblamo.xyz/repository.universalscrapers-1.0.0.zip
+wget http://kdil.co/repo/kodil.zip
+wget http://kdil.co/repo/repository.ah.zip
