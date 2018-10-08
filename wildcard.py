5455d329925f02db8ae4cdb6c0e78d7184f65f02
Evan Wappel <ewappel@gmail.com>
2018-10-07 18:03:59 -0700

    cerb

---
+++ wildcard.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+resp = urllib2.urlopen('http://noobsandnerds.com/portal/*REPOSITORY%20PORTAL')
+soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
+
+for link in soup.find_all('a', href=True):
+    print link['href']
